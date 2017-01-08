b3d4771f1c6f93e8708b6de255d6303df859a91a
davidfabara <dfffaba@hotmail.com>
2017-01-07 23:34:38 -0500

    Dados en clases

---
+++ JavaApplication/src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication3;
+
+import javax.swing.JOptionPane;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Acceso Total
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String resultados="";
+        JOptionPane.showMessageDialog(null,resultados,"Ensayando una nueva versión",JOptionPane.PLAIN_MESSAGE);
+        //nueva modificación
+        
+    }
+    
+        
+}
